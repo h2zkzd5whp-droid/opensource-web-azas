360ba59a287e61e643f3d2997954c1546d86b8c6
h2zkzd5whp-droid <h2zkzd5whp@privaterelay.appleid.com>
2026-03-24 20:11:53 +0900

    feat: register API 구현 (검증, 중복확인, bcrypt 해싱, DB 저장)

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,17 @@
-// TODO: 구현
+const pool = require('../config/db.js');
+
+exports.findByEmail = async function(email) {
+    const [rows] = await pool.query('SELECT * FROM Users WHERE email = ?', [email]);
+    return rows[0];
+}
+
+exports.create = async (email, hashedPassword, nickname) => {
+    const[result] = await pool.query('INSERT INTO Users (email, password, nickname, createdAt, updatedAt) VALUES (?, ?, ?, NOW(), NOW())', 
+        [email, hashedPassword, nickname]);
+    return result.insertId;
+}
+
+// TODO: findById - getMe용
+// TODO: update - updateMe용
+// TODO: updatePassword - changePassword용
+

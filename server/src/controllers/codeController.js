dd702f46ef536e116f5535f695f037052ffb2445
suda5936 <gustlr3308@naver.com>
2026-03-28 04:19:57 +0900

    feat: implement createCode (controller, model)

--- server/src/controllers/codeController.js
+++ server/src/controllers/codeController.js
@@ -1,3 +1,5 @@
+const Code = require('../models/Code');
+
 // TODO: 코드 실행 구현 (Judge0 CE API)
 exports.runCode = async (req, res, next) => {
   res.json({ message: 'runCode - TODO' });
@@ -5,7 +7,35 @@ exports.runCode = async (req, res, next) => {
 
 // TODO: 코드 저장 (신규) 구현
 exports.createCode = async (req, res, next) => {
-  res.json({ message: 'createCode - TODO' });
+  try {
+        const { language, source, title = 'Untitled'} = req.body;
+        const userId = req.user.userId; 
+
+        //데이터 검증
+        if (!language || !source) {
+            return res.status(400).json({ 
+                success: false, 
+                message: '언어와 소스 코드는 필수입니다.' 
+            });
+        }
+
+        //DB 저장
+        const result = await Code.create({
+            userId,
+            title,
+            language,
+            source
+        });
+
+        res.status(201).json({
+            message: '코드가 성공적으로 저장되었습니다.',
+            codeId: result.insertId,
+            createdAt :result.createdAt
+        });
+
+      }catch(err) {
+      next(err);
+      }
 };
 
 // TODO: 코드 목록 구현
--- server/src/models/Code.js
+++ server/src/models/Code.js
@@ -1,1 +1,16 @@
-// TODO: 구현
+const pool = require('../config/db.js');
+
+exports.create = async (userId, title, language, source) => {
+    const [result] = await pool.query(
+        'INSERT INTO Codes (userId, title, language, source) VALUES (?, ?, ?, ?)', 
+        [userId, title, language, source]
+    );
+
+    // get createdAt
+    const [rows] = await pool.query(
+        'SELECT codeId, createdAt FROM Codes WHERE codeId = ?', 
+        [result.insertId]
+    );
+
+    return rows[0]; 
+};
